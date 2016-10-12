bf7504e560faf862d57d97473d576a314a91aac4
Arnaud PEZEL <arnaud@apyx.fr>
2016-10-12 16:23:07 +0200

    -

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="ember" />
+export declare function EmberChain(constructor: Function): any;
+export declare function on(...events: string[]): (target: any, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) => any;
+export declare function observes(...props: string[]): (target: any, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) => any;
+export declare function computed(...props: string[]): (target: any, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) => any;
---
+++ index.js
@@ -0,0 +1,31 @@
+import * as Ember from 'Ember';
+export function EmberChain(constructor) {
+    const mixin = Ember.Mixin.create(), parent = Object.getPrototypeOf(constructor);
+    mixin.properties = mixin.properties || {};
+    Object.getOwnPropertyNames(constructor.prototype).forEach((prop) => {
+        if (prop !== 'constructor') {
+            mixin.properties[prop] = constructor.prototype[prop];
+        }
+    });
+    return parent.extend(mixin);
+}
+export function on(...events) {
+    return function (target, propertyKey, descriptor) {
+        descriptor.value.__ember_listens__ = events;
+        return descriptor;
+    };
+}
+export function observes(...props) {
+    return function (target, propertyKey, descriptor) {
+        props.push(descriptor.value);
+        descriptor.value = Ember.observer("viz", descriptor.value);
+        return descriptor;
+    };
+}
+export function computed(...props) {
+    return function (target, propertyKey, descriptor) {
+        let ret = Ember.computed(descriptor.value);
+        descriptor.value = ret.property(...props);
+        return descriptor;
+    };
+}
